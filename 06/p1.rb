a5f16cea2a304e65ce9a6ac2c8c33c69e6dcb776
ade ishs <ade@adeishs.com>
2023-12-06 13:14:53 +0700

    Shorten code

--- 06/p1.rb
+++ 06/p1.rb
@@ -2,12 +2,11 @@
 # frozen_string_literal: true
 
 def parse_input(input_str)
-  ts, ds = input_str.split("\n")
-                    .map { |l| l.split(':').last.strip.split(/ +/).map(&:to_i) }
+  input_str.split("\n")
+           .map { |l| l.split(':').last.strip.split(/ +/).map(&:to_i) }
 end
 
 ts, ds = parse_input($stdin.read)
-wins = ts.map.with_index { |t, i|
+puts ts.map.with_index { |t, i|
   [*0..t].select { |h| h * (t - h) > ds[i] }.size
-}
-puts wins.reduce(:*)
+}.reduce(:*)
