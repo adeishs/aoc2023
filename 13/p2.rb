db5aacebcdd700df86ac8f4520f9aae1ce8c5805
ade ishs <ade@adeishs.com>
2023-12-13 21:38:32 +1100

    Remove unnecessary conversions

--- 13/p2.rb
+++ 13/p2.rb
@@ -1,15 +1,15 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-def get_pos_char(row)
-  row.chars.map.with_index { |c, i| [i, c] }.to_h
+def get_pos_chars(row)
+  row.chars.map.with_index { |c, i| [i, c] }
 end
 
 def reflection?(rows_pair)
   cnt = 0
   bs = rows_pair.last.reverse
   rows_pair.first.each_with_index do |a, i|
-    cnt += (get_pos_char(a).to_a - get_pos_char(bs[i]).to_a).to_h.size
+    cnt += (get_pos_chars(a) - get_pos_chars(bs[i])).to_h.size
     return false if cnt > 1
   end
 
