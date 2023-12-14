1a6a379fb8efd08de4a65bfb6a89ed5313673d4d
ade ishs <ade@adeishs.com>
2023-12-14 19:01:56 +1100

    Shorten code

--- 14/p1.rb
+++ 14/p1.rb
@@ -3,31 +3,28 @@
 
 ROUNDED = 'O'
 CUBE = '#'
-EMPTY = '.'
 
 # sum of arithmetic series (d = –1):
-def series_sum(a, n)
-  (2 * a + 1 - n) * n / 2
+def series_sum(s_start, num)
+  (2 * s_start + 1 - num) * num / 2
 end
 
-def get_series_round_count(line, s_idx, e_idx)
-  (s_idx..e_idx).find_all { |i| line[i] == ROUNDED }.size
+def get_series_round_count(line, range)
+  range.find_all { |i| line[i] == ROUNDED }.size
+end
+
+def get_cube_idxs(line)
+  # add pretend cube rocks at the edges
+  [-1].concat((0...line.size).find_all { |i| line[i] == CUBE })
+      .append(line.size)
 end
 
 def get_line_load(line)
-  c_idxs = (0...line.size).find_all { |i| line[i] == CUBE }
-  prev = -1
-  c_idxs.map do |curr|
-    s = prev + 1
-    r_cnt = get_series_round_count(line, s, curr - 1)
-    prev = curr
-    series_sum(line.size - s, r_cnt)
-  end.append(
-    series_sum(
-      line.size - (prev + 1),
-      get_series_round_count(line, prev + 1, line.size - 1)
-    )
-  ).sum
+  c_idxs = get_cube_idxs(line)
+  (1...c_idxs.size).map do |i|
+    s = c_idxs[i - 1] + 1
+    series_sum(line.size - s, get_series_round_count(line, s..c_idxs[i] - 1))
+  end.sum
 end
 
 def solve(rows)
