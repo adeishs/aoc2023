bd0ddff090775619742a6c5e1752a70f4e6357f2
ade ishs <ade@adeishs.com>
2023-12-14 19:30:08 +1100

    Do less arithmetic and rename to prep for part 2

--- 14/p1.rb
+++ 14/p1.rb
@@ -22,15 +22,15 @@ end
 def get_line_load(line)
   c_idxs = get_cube_idxs(line)
   (1...c_idxs.size).map do |i|
-    s = c_idxs[i - 1] + 1
-    series_sum(line.size - s, get_series_round_count(line, s...c_idxs[i]))
+    s = c_idxs.shift + 1
+    series_sum(line.size - s, get_series_round_count(line, s...c_idxs.first))
   end.sum
 end
 
-def solve(rows)
+def get_total_load(rows)
   rows.map(&:chars).transpose
       .map { |line| get_line_load(line) }
       .sum
 end
 
-puts solve($stdin.read.split("\n"))
+puts get_total_load($stdin.read.split("\n"))
