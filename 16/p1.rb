69eafed1f1b1c1ac9bdba37f141887d49163a694
ade ishs <ade@adeishs.com>
2023-12-16 18:47:54 +1100

    Add day 16 part 1

---
+++ 16/p1.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'set'
+
+def run_beam(tiles, start, dir, energs)
+  curr = start
+  until curr.rect.include?(-1) ||
+        curr.imag == tiles.size ||
+        curr.real == tiles.first.size
+    energs[curr] = Set.new if energs[curr].nil?
+    break if energs[curr].member?(dir)
+
+    energs[curr] = energs[curr] << dir
+    t = tiles[curr.imag][curr.real]
+    if t == '-' && dir.real.zero?
+      [-1, 1].each { |r| run_beam(tiles, curr + r, r, energs) }
+      break
+    elsif t == '|' && dir.imag.zero?
+      [-1, 1].map { |i| Complex(0, i) }
+             .each { |d| run_beam(tiles, curr + d, d, energs) }
+      break
+    end
+
+    if (t == '/' && dir.real.zero?) || (t == '\\' && dir.imag.zero?)
+      dir *= 0 + 1i
+    elsif (t == '/' && dir.imag.zero?) || (t == '\\' && dir.real.zero?)
+      dir *= 0 + -1i
+    end
+
+    curr += dir
+  end
+end
+
+tiles = $stdin.each_line
+              .map(&:chomp)
+energs = {}
+run_beam(tiles, 0 + 0i, 1 + 0i, energs)
+puts energs.size
